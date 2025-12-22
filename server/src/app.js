01f49da03d51d57db5219c6407310947e6782f10
“aashi-336” <ayushisongara336@gmail.com>
2025-12-22 22:14:45 +0530

    Add MongoDB connection and authentication module

---
+++ server/src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import authRoutes from "./modules/auth/auth.routes.js";
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+app.use("/api/auth", authRoutes);
+export default app;
---
+++ server/src/modules/auth/auth.controller.js
@@ -0,0 +1,49 @@
+import User from "./auth.model.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser)
+      return res.status(400).json({ message: "User already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    res.status(201).json({ message: "User created successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
